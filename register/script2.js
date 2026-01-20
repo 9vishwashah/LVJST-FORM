38689553a22f2abbe29736ce96c63b7cdc560871
unknown <9vishwashah@gmail.com>
2026-01-20 10:22:21 +0530

    UpdatedSceipt

--- register/script2.js
+++ register/script2.js
@@ -261,7 +261,7 @@ async function getRecaptchaToken(action = 'submit_volunteer') {
 // --- call server function ---
 async function callSubmitVolunteerFunction(volunteerData, recaptchaToken = null) {
     const payload = { ...volunteerData, recaptchaToken };
-    const url = '/.netlify/functions/submitVolunteer'; 
+    const url = './netlify/functions/submitVolunteer'; 
     const resp = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
